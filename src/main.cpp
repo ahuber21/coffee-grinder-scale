fe428b1780b623e0e3a17f9e3a093e6078444606
Andreas Huber <ahuber1121@gmail.com>
2025-04-24 09:50:10 +0200

    stable tare

--- src/main.cpp
+++ src/main.cpp
@@ -46,26 +46,26 @@ static const unsigned long button_debounce_min_hold = 20;
 bool grinder_is_running = false;
 
 float target_grams = 0;
-float target_grams_corrected = 0; // includes the correction dose
+float target_grams_corrected = 0;  // includes the correction dose
 
 // various millis to keep track of when stuff happened
 unsigned long button_pressed_filter_millis = 0;
 unsigned long button_pressed_millis = 0;
 unsigned long debug_last_print_millis = 0;
 unsigned long finalize_millis = 0;
-unsigned long grinder_runtime_millis = 0; // how long the grinder was on for
-unsigned long grinder_started_millis = 0; // when the grinder was started
-unsigned long grinder_stopped_millis = 0; // when the grinder was stopped
+unsigned long grinder_runtime_millis = 0;  // how long the grinder was on for
+unsigned long grinder_started_millis = 0;  // when the grinder was started
+unsigned long grinder_stopped_millis = 0;  // when the grinder was stopped
 unsigned long last_grams_millis = 0;
 unsigned long last_heartbeat_millis = 0;
 unsigned long last_top_up_millis = 0;
-unsigned long session_started_millis = 0; // when the grind session was started
+unsigned long session_started_millis = 0;  // when the grind session was started
 unsigned long state_change_to_idle_millis = 0;
 unsigned long stopping_last_millis = 0;
 unsigned long top_up_stop_millis = 0;
 
 // various grams values to calculate differences between iterations
-float grams_on_grinder_on = 0.0f; // grams when grinder was turned on
+float grams_on_grinder_on = 0.0f;  // grams when grinder was turned on
 float last_grams = 0.0f;
 float stopping_last_grams = 0.0f;
 float top_up_grams_delta = 0.0f;
@@ -83,6 +83,7 @@ volatile enum State {
   BUTTON_FILTER,
   BUTTON_PRESSED,
   CONFIRM,
+  TARE,
   CONFIGURED,
   RUNNING,
   TOPUP,
@@ -102,8 +103,8 @@ enum ButtonPin {
   back = BUTTON_BACK,
 };
 
-volatile ButtonPin button;      // the button that was currently pressed
-volatile ButtonPin last_button; // the button that was previously pressed
+volatile ButtonPin button;       // the button that was currently pressed
+volatile ButtonPin last_button;  // the button that was previously pressed
 
 void setupDisplay();
 void setupWifi();
@@ -114,6 +115,7 @@ void loopButtonFilter();
 void loopButtonPressed();
 void loopButtonPressedDebug();
 void loopConfirm();
+void loopTare();
 void loopConfigured();
 void loopRunning();
 void loopTopUp();
@@ -128,7 +130,8 @@ void heartbeat();
 void grinderOn();
 void grinderOff();
 
-template <ButtonPin pin> void button_interrupt() {
+template <ButtonPin pin>
+void button_interrupt() {
   auto now = millis();
   button_pressed_filter_millis = now;
 
@@ -139,7 +142,7 @@ template <ButtonPin pin> void button_interrupt() {
   if (state == CONFIRM) {
     // go to configured if same button is pressed again
     // go to idle if the button is different
-    state = (pin == last_button) ? CONFIGURED : IDLE;
+    state = (pin == last_button) ? TARE : IDLE;
   } else if (state == IDLE) {
     old_state_button_press = IDLE;
     state = BUTTON_FILTER;
@@ -247,45 +250,48 @@ void loop() {
   }
 
   switch (state) {
-  case IDLE:
-    loopIdle();
-    break;
-  case BUTTON_FILTER:
-    loopButtonFilter();
-    break;
-  case BUTTON_PRESSED:
-    if (old_state_button_press == DEBUG) {
-      loopButtonPressedDebug();
-    } else if (old_state_button_press == IDLE) {
-      loopButtonPressed();
-    } else {
-      // ignore button press
-      state = old_state_button_press;
-    }
-    break;
-  case CONFIRM:
-    loopConfirm();
-    break;
-  case CONFIGURED:
-    loopConfigured();
-    break;
-  case RUNNING:
-    loopRunning();
-    break;
-  case TOPUP:
-    loopTopUp();
-    break;
-  case STOPPING:
-    loopStopping();
-    break;
-  case FINALIZE:
-    loopFinalize();
-    break;
-  case DEBUG:
-    loopDebug();
-    break;
-  default:
-    break;
+    case IDLE:
+      loopIdle();
+      break;
+    case BUTTON_FILTER:
+      loopButtonFilter();
+      break;
+    case BUTTON_PRESSED:
+      if (old_state_button_press == DEBUG) {
+        loopButtonPressedDebug();
+      } else if (old_state_button_press == IDLE) {
+        loopButtonPressed();
+      } else {
+        // ignore button press
+        state = old_state_button_press;
+      }
+      break;
+    case CONFIRM:
+      loopConfirm();
+      break;
+    case TARE:
+      loopTare();
+      break;
+    case CONFIGURED:
+      loopConfigured();
+      break;
+    case RUNNING:
+      loopRunning();
+      break;
+    case TOPUP:
+      loopTopUp();
+      break;
+    case STOPPING:
+      loopStopping();
+      break;
+    case FINALIZE:
+      loopFinalize();
+      break;
+    case DEBUG:
+      loopDebug();
+      break;
+    default:
+      break;
   }
 }
 
@@ -293,39 +299,42 @@ void heartbeat() {
   if (millis() - last_heartbeat_millis > 5000) {
     String message = "[heartbeat] state=";
     switch (state) {
-    case IDLE:
-      message += "IDLE";
-      break;
-    case BUTTON_FILTER:
-      message += "BUTTON_FILTER";
-      break;
-    case BUTTON_PRESSED:
-      message += "BUTTON_PRESSED";
-      break;
-    case CONFIGURED:
-      message += "CONFIGURED";
-      break;
-    case CONFIRM:
-      message += "CONFIRM";
-      break;
-    case RUNNING:
-      message += "RUNNING";
-      break;
-    case TOPUP:
-      message += "TOPUP";
-      break;
-    case STOPPING:
-      message += "STOPPING";
-      break;
-    case FINALIZE:
-      message += "FINALIZE";
-      break;
-    case DEBUG:
-      message += "DEBUG";
-      break;
-    default:
-      message += "UNHANDLED STATE: " + String((int)state);
-      break;
+      case IDLE:
+        message += "IDLE";
+        break;
+      case BUTTON_FILTER:
+        message += "BUTTON_FILTER";
+        break;
+      case BUTTON_PRESSED:
+        message += "BUTTON_PRESSED";
+        break;
+      case CONFIGURED:
+        message += "CONFIGURED";
+        break;
+      case CONFIRM:
+        message += "CONFIRM";
+        break;
+      case TARE:
+        message += "TARE";
+        break;
+      case RUNNING:
+        message += "RUNNING";
+        break;
+      case TOPUP:
+        message += "TOPUP";
+        break;
+      case STOPPING:
+        message += "STOPPING";
+        break;
+      case FINALIZE:
+        message += "FINALIZE";
+        break;
+      case DEBUG:
+        message += "DEBUG";
+        break;
+      default:
+        message += "UNHANDLED STATE: " + String((int)state);
+        break;
     }
     logger.println(message);
     last_heartbeat_millis = millis();
@@ -361,6 +370,9 @@ void loopIdle() {
     display.shutDown();
   } else {
     float grams = scale.getUnits();
+    if ((-0.3 < grams) && (grams < 0.3)) {
+      grams = 0.0f;
+    }
     display.displayString(String(grams, GRAMS_DIGITS) + " g",
                           VerticalAlignment::CENTER);
   }
@@ -390,41 +402,41 @@ void loopButtonPressed() {
   button_pressed_millis = millis();
 
   switch (button) {
-  case left:
-    target_grams = settings.scale.target_dose_single;
-    target_grams_corrected =
-        settings.scale.target_dose_single - settings.scale.top_up_margin_single;
-    state = CONFIRM;
-    break;
-  case right:
-    target_grams = settings.scale.target_dose_double;
-    target_grams_corrected =
-        settings.scale.target_dose_double - settings.scale.top_up_margin_double;
-    state = CONFIRM;
-    break;
-  case back:
-    state = DEBUG;
-    break;
-  default:
-    state = IDLE;
-    break;
+    case left:
+      target_grams = settings.scale.target_dose_single;
+      target_grams_corrected = settings.scale.target_dose_single -
+                               settings.scale.top_up_margin_single;
+      state = CONFIRM;
+      break;
+    case right:
+      target_grams = settings.scale.target_dose_double;
+      target_grams_corrected = settings.scale.target_dose_double -
+                               settings.scale.top_up_margin_double;
+      state = CONFIRM;
+      break;
+    case back:
+      state = DEBUG;
+      break;
+    default:
+      state = IDLE;
+      break;
   }
 }
 
 void loopButtonPressedDebug() {
   switch (button) {
-  case left:
-    state = DEBUG;
-    break;
-  case right:
-    state = DEBUG;
-    break;
-  case back:
-    state = IDLE;
-    break;
-  default:
-    state = IDLE;
-    break;
+    case left:
+      state = DEBUG;
+      break;
+    case right:
+      state = DEBUG;
+      break;
+    case back:
+      state = IDLE;
+      break;
+    default:
+      state = IDLE;
+      break;
   }
 }
 
@@ -439,15 +451,22 @@ void loopConfirm() {
   }
 }
 
+void loopTare() {
+  // we tare the scale until we get a stable reading
+  display.displayString("T", VerticalAlignment::CENTER);
+  bool success = scale.tare();
+  if (success) {
+    // averages over ring buffer were stable, move on
+    state = CONFIGURED;
+  }
+}
+
 void loopConfigured() {
   // reset graph
   graph.resetGraph(target_grams);
   graph.updateGraphData(0.0f, 0.0f);
 
   display.displayString("T", VerticalAlignment::CENTER);
-
-  // tare
-  scale.tare();
 
   // start grinder
   grinderOn();
